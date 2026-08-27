773dbc20814b476a0f2f3c34623885de50ee1f83
nuthan22devops <nuthankrishna22devops@gmail.com>
2026-08-27 21:36:55 +0530

    Update test_app.py

--- services/anime-service/tests/test_app.py
+++ services/anime-service/tests/test_app.py
@@ -1,6 +1,11 @@
-from fastapi.testclient import TestClient
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
 from app import app
+from fastapi.testclient import TestClient
+
 
 client = TestClient(app)
 
